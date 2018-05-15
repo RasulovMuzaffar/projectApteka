d02cd5d159c741551cf5e976efa324166e89c9c4
Muzaffar <rasulovmuzaffar@gmail.com>
2018-05-15 09:33:51 +0500

    add adminPage

---
+++ src/main/java/sample/ui/admin/AdminController.java
@@ -0,0 +1,4 @@
+package sample.ui.admin;
+
+public class AdminController {
+}
--- src/main/java/sample/ui/login/LoginController.java
+++ src/main/java/sample/ui/login/LoginController.java
@@ -1,15 +1,21 @@
 package sample.ui.login;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import sample.database.DatabaseHandler;
 import sample.model.dao.StaffDao;
 import sample.model.dao.StaffDaoImpl;
 import sample.model.pojo.Staff;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -23,7 +29,7 @@ public class LoginController {
     public PasswordField txtPassword;
     public Button btnLogin;
 
-    public void btnLoginAction(ActionEvent event) {
+    public void btnLoginAction(ActionEvent event) throws IOException {
 
         StaffDao sd = new StaffDaoImpl();
         Staff staff = sd.getStaffByLoginPassword(txtLogin.getText(), txtPassword.getText());
@@ -32,6 +38,13 @@ public class LoginController {
             switch (staff.getIdRole()) {
                 case 1:
                     lblInfo.setText("very good! you are system admin!" + txtLogin.getText());
+                    //Close current
+//                    Stage stage = (Stage) button.getScene().getWindow();
+                    // do what you have to do
+                    Stage stage = (Stage) btnLogin.getScene().getWindow();
+                    stage.close();
+                    Stage stage1 = new Stage();
+                    getAdminPage(stage1);
                     break;
                 case 2:
                     lblInfo.setText("very good! you are manager!" + txtLogin.getText());
@@ -46,5 +59,15 @@ public class LoginController {
         } else {
             lblInfo.setText("invalid login or password!!!");
         }
+    }
+
+    private void getAdminPage(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/sample/ui/admin/admin.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Другая форма");
+        stage.setScene(new Scene(root1));
+        stage.show();
     }
 }
