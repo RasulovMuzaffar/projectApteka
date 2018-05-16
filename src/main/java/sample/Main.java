bb88e29f941a3f02fa302c9fca7d3c4305d7a4d1
serverlipe <serverlipe@gmail.com>
2018-05-16 10:23:16 +0500

    переход по страницам

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -10,13 +10,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample/ui/login/login.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("/sample/ui/login/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/ui/statistic/statistic.fxml"));
         primaryStage.setTitle("Authorization");
         primaryStage.setScene(new Scene(root, 635, 450));
-        primaryStage.setMinWidth(635);
-        primaryStage.setMinHeight(450);
-        primaryStage.setMaxWidth(635);
-        primaryStage.setMaxHeight(450);
+        primaryStage.setMaximized(true);
+        //primaryStage.setMinWidth(635);
+        //        primaryStage.setMinHeight(450);
+        //        primaryStage.setMaxWidth(635);
+        //        primaryStage.setMaxHeight(450);
         primaryStage.show();
     }
 
--- src/main/java/sample/ui/statistic/Statistic.java
+++ src/main/java/sample/ui/statistic/Statistic.java
@@ -1,14 +1,48 @@
 package sample.ui.statistic;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Statistic {
-    public JFXButton statik;
-    public JFXButton kirim;
-    public JFXButton chiqim;
-    public JFXButton kop;
-    public JFXButton muddati;
-    public JFXButton mahsuot;
-    public JFXButton xodimlar;
-    public JFXButton mahsulotR;
+public class Statistic{
+
+        public JFXButton statistic;
+        public JFXButton kirim;
+        public JFXButton kup;
+        public JFXButton chiqim;
+        public JFXButton muddati;
+        public JFXButton skidka;
+        public JFXButton xodimlar;
+        public JFXButton mahsulot;
+
+        @FXML
+        public void initialize(){
+            nexter(chiqim, "/sample/ui/chiqim/chiqim.fxml");
+            nexter(kirim, "/sample/ui/kirim/kirim.fxml");
+            nexter(kup, "/sample/ui/kop/kop.fxml");
+            nexter(skidka, "/sample/ui/mahsulot/mahulot.fxml");
+            nexter(mahsulot, "/sample/ui/mahsulotlar/mahsulotlar.fxml");
+            nexter(muddati, "/sample/ui/muddat/muddat.fxml");
+            nexter(xodimlar, "/sample/ui/ruyxatXodim/xodimlar.fxml");
+            nexter(statistic, "/sample/ui/statistic/statistic.fxml");
+        }
+    private void nexter(JFXButton mahsulot, String s) {
+        mahsulot.setOnAction(event -> {
+            System.out.println(s);
+            try{
+                System.out.println(mahsulot.getText());
+                Parent blah = FXMLLoader.load(getClass().getResource(s));
+                Scene scene = new Scene(blah);
+                Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                appStage.setScene(scene);
+                appStage.show();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+    }
 }
